916267403e5c7881a1cd12a7e442391a03e89330
root <root@peakservers.anyshpm.info>
2018-05-06 11:42:29 +0800

    add hostname

--- configure.sh
+++ configure.sh
@@ -21,7 +21,8 @@ passwd anyshpm
 echo 'anyshpm	ALL=(ALL)	ALL' > /etc/sudoers.d/anyshpm
 
 #hostname
-hostnamectl set-hostname peakservers.anyshpm.info
+read hostname
+hostnamectl set-hostname $hostname
 
 #zabbix
 rpm -i http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm
